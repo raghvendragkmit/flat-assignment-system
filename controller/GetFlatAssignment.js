f8bb3bbe23232aa653437933c2df7611dad87b00
raghvendragkmit <raghvendra@gkmit.co>
2022-10-16 22:53:45 +0530

    Updated  GetFlatAssignment Controller

--- controller/GetFlatAssignment.js
+++ controller/GetFlatAssignment.js
@@ -2,7 +2,7 @@ const db = require("../connection");
 
 exports.getFlatAssignment = (req,res)=>
 {
-    db.query("select User.id, User.firstName, User.lastName, User.email, FlatAssignment.flatId, FlatAssignment.date, FlatAssignment.rent From FlatAssignment Inner join User on User.id = FlatAssignment.userId",(err,result)=>
+    db.query("select User.id, User.firstName, User.lastName, User.email, FlatAssignment.flatId, Flat.floor, Flat.size, FlatAssignment.date, FlatAssignment.rent From ((FlatAssignment Inner join User on User.id = FlatAssignment.userId) Inner join Flat on Flat.id = FlatAssignment.flatId)",(err,result)=>
     {
         if(err)
         return res.status(503).json({message : "Please try after sometime"});
